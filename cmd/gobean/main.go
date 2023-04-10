b4409c736abd3a390954c29370f62ccd7d65e242
snowmerak <snowmerak@outlook.com>
2023-04-10 22:48:14 +0900

    make cmd

---
+++ cmd/gobean/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"github.com/snowmerak/go-bean/internal/finder"
+	"github.com/snowmerak/go-bean/internal/generator"
+	"github.com/snowmerak/go-bean/internal/model"
+	"go/format"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	models := []model.Model(nil)
+
+	if err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if filepath.Ext(path) != ".go" {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		models = append(models, finder.Find(f))
+
+		return nil
+	}); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", models)
+
+	bs, err := generator.Generate(models...)
+	if err != nil {
+		panic(err)
+	}
+
+	rs, err := format.Source(bs)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := os.MkdirAll("bean", os.ModePerm); err != nil {
+		panic(err)
+	}
+
+	f, err := os.Create("bean/bean.go")
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := f.Write(rs); err != nil {
+		panic(err)
+	}
+
+	if err := f.Close(); err != nil {
+		panic(err)
+	}
+}
