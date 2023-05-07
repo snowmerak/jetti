5299d6590664d88e38a53ccfef8984bff385a95d
snowmerak <snowmerak@outlook.com>
2023-05-07 14:47:54 +0900

    make example

---
+++ cmd/prac/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/snowmerak/jetti/lib/generator"
+	"github.com/snowmerak/jetti/lib/model"
+)
+
+func main() {
+	pkg := model.Package{
+		Name: "main",
+		Imports: []model.Import{
+			{
+				Path: "fmt",
+			},
+		},
+		Structs: []model.Struct{
+			{
+				Name: "Person",
+				Fields: []model.Field{
+					{
+						Name: "Name",
+						Type: "string",
+					},
+					{
+						Name: "Age",
+						Type: "int",
+					},
+				},
+			},
+		},
+		Functions: []model.Function{
+			{
+				Name: "main",
+				Code: []string{
+					"p := Person{Name: \"John\", Age: 20}",
+					"p.Name = \"Jack\"",
+					"p.Age = 30",
+					"fmt.Println(p)",
+				},
+			},
+		},
+	}
+
+	rs, err := generator.GenerateFile(&pkg)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(rs))
+}
