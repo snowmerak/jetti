12b9ef553f180073a676189342143583db9c00fd
snowmerak <snowmerak@outlook.com>
2023-05-10 23:47:05 +0900

    add new and run command package

--- internal/executor/cli/cli.go
+++ internal/executor/cli/cli.go
@@ -3,21 +3,19 @@ package cli
 const (
 	Generate = "generate"
 	New      = "new <module-name>"
+	Run      = "run <command-name>"
+	RunArgs  = "run <command-name> <args>"
 )
 
-/*
-TODO: add proto
-TODO: add fbs
-TODO: add bean
-TODO: add cmd
-TODO: add pprof
-TODO: add server
-TODO: add json/yaml
-*/
 type CLI struct {
 	Generate struct {
 	} `cmd:"" help:"Generate code"`
 	New struct {
 		ModuleName string `arg:"" help:"Module name"`
+		Cmd        bool   `cmd:"" help:"Create a new command"`
 	} `cmd:"" help:"Create a new project"`
+	Run struct {
+		CommandName string   `arg:"" help:"Command name"`
+		Args        []string `arg:"" optional:"" help:"Command arguments"`
+	} `cmd:"" help:"Run a command"`
 }
