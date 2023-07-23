bf08f9fa3a5fdfaa435605e64961df722ebdf697
snowmerak <snowmerak@outlook.com>
2023-07-23 18:33:34 +0900

    fix bug

--- internal/executor/generate/errface.go
+++ internal/executor/generate/errface.go
@@ -27,27 +27,38 @@ func ErrFace(root string, getters []check.Getter) error {
 					defer f.Close()
 
 					pkg := &model.Package{
-						Name:       getter.PackageName,
+						Name:       "errface",
 						Imports:    getter.Imports,
 						Interfaces: []model.Interface{},
 					}
 
+					pascalPackageName := strings.ToUpper(getter.PackageName[:1]) + getter.PackageName[1:]
+
+					wholeMethods := make([]model.Method, 0)
+
 					for i, fieldName := range structData.FieldNames {
 						fieldName := strings.ToUpper(fieldName[:1]) + fieldName[1:]
-						pkg.Interfaces = append(pkg.Interfaces, model.Interface{
-							Name: fmt.Sprintf("Get%s", fieldName),
-							Methods: []model.Method{
-								{
-									Name: "Get" + fieldName,
-									Return: []model.Field{
-										{
-											Type: structData.FieldTypes[i],
-										},
+						methods := []model.Method{
+							{
+								Name: "Get" + fieldName,
+								Return: []model.Field{
+									{
+										Type: structData.FieldTypes[i],
 									},
 								},
 							},
+						}
+						pkg.Interfaces = append(pkg.Interfaces, model.Interface{
+							Name:    fmt.Sprintf("Get%sOf%s%s", fieldName, pascalPackageName, structName),
+							Methods: methods,
 						})
+						wholeMethods = append(wholeMethods, methods...)
 					}
+
+					pkg.Interfaces = append(pkg.Interfaces, model.Interface{
+						Name:    fmt.Sprintf("Get%s%s", pascalPackageName, structName),
+						Methods: wholeMethods,
+					})
 
 					value, err := generator.GenerateFile(pkg)
 					if err != nil {
