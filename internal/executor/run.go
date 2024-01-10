2170b65bb258483228cdcbdf9a608a959e310d32
snowmerak <snowmerak@outlook.com>
2024-01-10 22:03:51 +0900

    add clib

--- internal/executor/run.go
+++ internal/executor/run.go
@@ -1,8 +1,11 @@
 package executor
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
 )
 
 func Run(root string, name string, args ...string) error {
@@ -12,6 +15,12 @@ func Run(root string, name string, args ...string) error {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
+	if _, err := os.ReadDir(filepath.Join(root, "clib")); !os.IsNotExist(err) {
+		cmd.Env = append(os.Environ(), "CGO_ENABLED=1")
+		cmd.Env = append(cmd.Env, fmt.Sprintf("CGO_LDFLAGS=-L%s/clib/%s-%s/lib", root, runtime.GOOS, runtime.GOARCH))
+		cmd.Env = append(cmd.Env, fmt.Sprintf("CGO_CFLAGS=-I%s/clib/%s-%s/include", root, runtime.GOOS, runtime.GOARCH))
+	}
 
 	if err := cmd.Run(); err != nil {
 		return err
